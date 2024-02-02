8318141f9211bad65a382b5e96aa20b0d765dec4
a-ok123 <a-ok123@gmail.com>
2024-02-02 00:46:30 -0500

    Added other OSs

--- examples/sd.py
+++ examples/sd.py
@@ -1,5 +1,4 @@
-import time
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler, DiffusionPipeline
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 import torch
 from compel import Compel
 import json
@@ -16,6 +15,7 @@ prompt = """
 def json_to_compel_prompt(json_prompt):
     json_data = json.loads(json_prompt)
     flat_list = []
+
     def flatten_element(key, value):
         if isinstance(value, dict):
             for sub_key, sub_value in value.items():
@@ -29,9 +29,11 @@ def json_to_compel_prompt(json_prompt):
     # return '("{0}").and()'.format("\", \"".join(flat_list))
     return ",".join(flat_list)
 
+
 def flatten_json_prompt(json_prompt):
     json_data = json.loads(json_prompt)
     flat_list = []
+
     def flatten_element(key, value):
         if isinstance(value, dict):
             for sub_key, sub_value in value.items():
@@ -44,11 +46,16 @@ def flatten_json_prompt(json_prompt):
         flatten_element(k, v)
     return ", ".join(flat_list)
 
-# Use the Euler scheduler here instead
+use_gpu = False
+if use_gpu:
+    dtype = torch.float16
+else:
+    dtype = torch.float32
 scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
-# pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, custom_pipeline="lpw_stable_diffusion", scheduler=scheduler)
-pipe = pipe.to("cuda")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=dtype)
+if use_gpu:
+    pipe = pipe.to("cuda")
+
 
 neg_prompt = "lowres, bad_anatomy, error_body, error_hair, error_arm, error_hands, bad_hands, error_fingers, bad_fingers, missing_fingers, error_legs, bad_legs, multiple_legs, missing_legs, error_lighting, error_shadow, error_reflection, text, error, extra_digit, fewer_digits, cropped, worst_quality, low_quality, normal_quality, jpeg_artifacts, signature, watermark, username, blurry"
 prompt = flatten_json_prompt(prompt)
@@ -60,12 +67,5 @@ compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
 conditioning = compel(prompt)
 negative_conditioning = compel(neg_prompt)
 [conditioning, negative_conditioning] = compel.pad_conditioning_tensors_to_same_length([conditioning, negative_conditioning])
-image = pipe(prompt_embeds=conditioning, negative_propmt_embeds=negative_conditioning, num_inference_steps=1000).images[0]
+image = pipe(prompt_embeds=conditioning, negative_propmt_embeds=negative_conditioning, num_inference_steps=50).images[0]
 image.save("testCompeln2-1.png")
-
-# with lpw_stable_diffusion
-# image = pipe.text2img(prompt, negative_prompt=neg_prompt, width=512,height=512,max_embeddings_multiples=3, num_inference_steps=1000).images[0]
-# image.save("testLPW.png")
-
-# image = pipe(prompt, num_inference_steps=1000).images[0]
-# image.save("test2.png")
